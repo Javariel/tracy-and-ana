4b45498533a36a0fc4f77236c3f508609737fffb
Javariel <ariel.xiezhe@gmail.com>
2022-08-25 16:13:56 +0200

    bootstrap project

---
+++ youtube-api/src/test/java/pr/xz/tracy/ana/youtube/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.xz.tracy.ana.youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
